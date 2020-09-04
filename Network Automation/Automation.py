79d10515f15aee1bc840e2b81c4f91e724d37855
kuhakuu04 <42318299+kuhakuu04@users.noreply.github.com>
2020-09-04 22:54:24 +0800

    fix error calling function

--- Network Automation/Automation.py
+++ Network Automation/Automation.py
@@ -1,12 +1,14 @@
 #module
 import os
 import paramiko
+import getpass
+from Router import Mikrotik
 
 os.system('clear')
 
 
 #login banner
-print('NETWORK AUTOMATION\nCREATE BY LNF LINFIQ 04\n\n\nhelp - show command\n')
+print('NETWORK AUTOMATION\nCREATE BY LNF LINFIQ 04\n\n\nver1.0(beta)\nhelp - show command\n')
 
 while True:
 
@@ -50,21 +52,71 @@ while True:
                 username = input('username\n>>> ')
                 password = input('password\n>>> ')
 
+                #connect module
+                Router_API = paramiko.SSHClient()
+                Router_API.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                Router_API.connect(hostname, port, username, password)
+                #stdin, stdout, stderr = Router_API.exec_command(router_command)
+                #Router_run_command = stdout.readlines()
+                #print(Router_run_command)
+
                 while True:
                     router_command = input('LNF(command-Mikrotik)> ')
 
-                    #connect module
-                    Router_API = paramiko.SSHClient()
-                    Router_API.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                    Router_API.connect(hostname, port, username, password)
-                    stdin, stdout, stderr = Router_API.exec_command(router_command)
-                    Router_run_command = stdout.readlines()
-                    print(Router_run_command)
+                    
 
-                    if(router_command == 'exit'):
+                    if(router_command == 'end'):
                         break
+
+                    elif(router_command == 'help'):
+                        print('basic_command - configure basic command for mikrotik access internet')
+                        print('clear - clear screen')
+                        print('end - back')
+                        print('help - show command')
+                        print('\n')
+
+                    elif(router_command == 'basic_command'):
+
+                        os.system('clear')
+
+                        #command for setting
+                        print('\nfill "ether1" if your router using port 1 for internet access\n')
+                        internet_port = input('insert internet port on your router\n>>> ')
+                        print('\n\ngive your router a name\n')
+                        hostname_router = input('insert hostname on your router\n>>> ')
+                        print('\n\nadd user for your router\n')
+                        user_name = input('add user on your router\n>>> ')
+                        user_pass = getpass.getpass('add password for your user\n>>> ')
+                        user_type = input('\nset the user type for your user(full, read, write)\n>>> ')
+                        choice_next = input('\nwhat is your router uses the dhcp client? y/n\n>>> ')
+
+                        #for calling package
+                        Basic_Setting_internet = Mikrotik.Basic_Configure.Set_Basic_Access(internet_port)
+                        Basic_Setting_hostname = Mikrotik.Basic_Configure.Set_Hostname(hostname_router)
+                        Basic_Setting_user = Mikrotik.Basic_Configure.Set_User(user_name, user_pass, user_type)
+
+                        stdin, stdout, stderr = Router_API.exec_command(Basic_Setting_internet + '\n' + Basic_Setting_hostname + '\n' + Basic_Setting_user)
+                        Router_Command_Call = stdout.readlines()
+
+                        if(choice_next == 'n'):
+                            network_address = input('insert your network address\n>>> ')
+                            gateway_address = input('insert your gateway address\n>>> ')
+                            Basic_Setting_Static_Router = Mikrotik.Basic_Configure.Set_Static_Route(network_address, gateway_address)
+
+                            stdin, stdout, stderr = Router_API.exec_command(Basic_Setting_Static_Router)
+
+                            Router_Command_Call = stdout.readlines()
+
+                        elif(choice_next == 'y'):
+                            continue
+
+                        else:
+                            print('command error')
+
                     else:
                         print('command error')
+
+
 
             #cisco brand
             elif(ssh_command == 'cisco'):
