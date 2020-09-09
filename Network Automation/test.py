fd973606eae2a9eb11e02c23ff0515fd7f4ef0c0
kuhakuu04 <mhudafiqri06@gmail.com>
2020-09-09 15:04:09 +0800

    testing command

--- Network Automation/test.py
+++ Network Automation/test.py
@@ -1,16 +1,6 @@
 import netmiko
 
-
-'''
-api = {
-    'device_type': 'cisco_ios',
-    'host': '173.10.3.1',
-    'username': 'cperoot',
-    'password': 'nasional123'
-}
-'''
-
-connect = netmiko.cisco.CiscoIosBase(ip='173.10.3.1', username='cperoot', password='nasional123')
+connect = netmiko.cisco.CiscoIosBase(ip='173.10.3.1', username='admin', password='Router123')
 
 output = connect.send_command('show users')
 
